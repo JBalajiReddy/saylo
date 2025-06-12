3b30a7f04bfe91b50df119eae7ed7a0c1bb356c6
JBalajiReddy <jeedipallybalajireddy@gmail.com>
2025-06-12 22:02:40 +0530

    added user, friends, chats endpoints

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
 
-import authRoute from "./routes/auth.route.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -9,7 +12,10 @@ const PORT = process.env.PORT;
 
 
 app.use(express.json());
-app.use("/api/auth", authRoute);
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 
 app.listen(PORT, () => {
